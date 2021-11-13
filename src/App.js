9dec9f397415358bcbc594c8d6507eb447266c48
Huy Le <huylhddev98@gmail.com>
2021-11-13 09:37:04 +0700

    update homepage and detail page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -8,26 +10,33 @@ import {
   Link
 } from "react-router-dom";
 import HomePage from './pages/HomePage';
+import Detail from './pages/Detail';
 
 function App() {
   return (
     <Router>
-      <div>
-        <nav class="main-nav">
-          <ul>
-            <li>
-              <Link to="/"><img class="nav-logo" src="logo.png" alt="logo" /></Link>
-            </li>
-          </ul>
+      <div class="pb-5">
+        <nav class="navbar justify-content-between px-md-5 px-3 mb-5">
+          <Link to="/"><img class="nav-logo" src="/logo.png" alt="logo" /></Link>
+          <span class="justify-content-center align-items-center">
+            <span class="me-2">Huy</span>
+            <img class="nav-avatar" src="/user-avatar.png" alt="avatar" />
+          </span>
         </nav>
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
+          <Route path="/detail/:productId">
+            <Detail />
+          </Route>
+          
           <Route path="/">
             <HomePage />
           </Route>
         </Switch>
+
+        <ToastContainer />
       </div>
     </Router>
   );
